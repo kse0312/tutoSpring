2fb0a670f67cfbf732c5657a23601b3d58c6205c
김승은 <tmddms0317@naver.com>
2023-06-02 10:45:44 +0900

    feat : init - .gitignore setting [intellij+all, java + add build, .gradle]

---
+++ src/main/java/com/example/tutoSpring/TutoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoSpringApplication.class, args);
+	}
+
+}
